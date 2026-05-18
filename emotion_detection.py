76380eb02ca879b4a799bc322f533df44b47320b
elizabeteella <elizabeteellazarina@gmail.com>
2026-05-18 12:29:12 +0100

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -18,6 +18,23 @@ def emotion_detector(text_to_analyze):
     if response.status_code == 200:
         formatted_response = response.json()
         emotions = formatted_response['emotionPredictions'][0]['emotion']
-        return emotions
+
+        anger = emotions['anger']
+        disgust = emotions['disgust']
+        fear = emotions['fear']
+        joy = emotions['joy']
+        sadness = emotions['sadness']
+
+        dominant_emotion = max(emotions, key=emotions.get)
+
+        return {
+            "anger": anger,
+            "disgust": disgust,
+            "fear": fear,
+            "joy": joy,
+            "sadness": sadness,
+            "dominant_emotion": dominant_emotion
+        }
+
     else:
         return None
